2031061580c01c8f39b8420e324e2347a3c5ff6c
masahiko.hosojima <zero_001@qc5.so-net.ne.jp>
2019-02-04 17:33:41 +0900

    HelloWorldのラベルを追加した。

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 細島誠彦 on 2019/02/04.
+//  Copyright © 2019 TransamManegemetnSystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
